dad6726ef698f4b86e9a5e7ae0ba18f54302cce9
Aryan009-sr <Prakharsingh9532@gmail.com>
2025-10-31 20:37:10 +0530

    fetching data using promise and Async-await

---
+++ JULY-LECTURES/6th_july_2025/script.js
@@ -0,0 +1,29 @@
+// Fetch and promise
+
+// const API = "https://jsonplaceholder.typicode.com/todos"
+
+// fetch (API)
+// .then((d) => {
+//     return d.json()
+// })
+// .then((resp) => {
+//     console.log(resp)
+// })
+// .catch((err) => {
+//     console.log(err)
+// })
+
+
+//Fetch with async await
+// const API2 = "https://jsonplaceholder.typicode.com/todos"
+
+// async function getData(){
+//     const response = await fetch(API2);
+//     const data = await response.json()
+//     console.log(data)
+//     // console.log(response) 
+// }
+// getData()
+
+
+
