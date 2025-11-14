4a2230f928c2971745ff8f11456637e8b3de4dba
Aryan009-sr <Prakharsingh9532@gmail.com>
2025-11-14 11:56:07 +0530

    Input eventListeners and value

---
+++ JULY-LECTURES/7th_july_2025/inputEvents/app.js
@@ -0,0 +1,15 @@
+
+
+let inpEl = document.querySelector('input')
+
+// inpEl.addEventListener('input', (e) => {
+//     console.log("Input Triggered")
+//     console.log(e.target.value)
+// });
+
+// inpEl.addEventListener('keydown', (event) => {
+//     console.log(event)
+//     console.log(event.target)
+//     console.log(event.target.value);
+// });
+
